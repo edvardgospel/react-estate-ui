876613fa4abf7cf912dd0ca245bb5ecd8ac1f220
Edvard Eros <edvard.eros@yahoo.com>
2024-06-13 17:32:06 +0200

    Profile Page

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,37 +1,55 @@
 import { useState } from "react";
-import './searchBar.scss';
+import "./searchBar.scss";
 
-const types = ['buy', 'rent'];
+const types = ["buy", "rent"];
 
 function SearchBar() {
-    const [query, setQuery] = useState({
-        'type': 'buy',
-        'location': '',
-        'minPrice': 0,
-        'maxPrice': 0
-    });
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
 
-    const switchType = (type) => {
-        setQuery({ ...query, type: type })
-    }
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
 
-    return (
-        <div className="searchBar">
-            <div className="type">
-                {types.map((type) => (
-                    <button key={type} onClick={() => switchType(type)} className={query.type === type ? "active" : ""}>{type}</button>
-                ))}
-            </div>
-            <form>
-                <input type="text" name="location" placeholder="City Location" />
-                <input type="number" name="minPrice" min={0} max={10000000} placeholder="Min Price" />
-                <input type="number" name="maxPrice" min={0} max={10000000} placeholder="Max Price" />
-                <button>
-                    <img src="/search.png" alt="" />
-                </button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
--- src/routes/layout/Layout.jsx
+++ src/routes/layout/Layout.jsx
@@ -1,18 +1,18 @@
-import './layout.scss';
+import "./layout.scss";
+import Navbar from "../../components/navbar/Navbar"
 import { Outlet } from "react-router-dom";
-import Navbar from "../../components/navbar/Navbar";
 
 function Layout() {
-    return (
-        <div className="layout">
-            <div className="navbar">
-                <Navbar />
-            </div>
-            <div className="content">
-                <Outlet />
-            </div>
-        </div>
-    );
+  return (
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Outlet/>
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
